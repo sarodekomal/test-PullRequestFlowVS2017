e2ea61a01470bea600a100dc63d68ec696a33270
poonampatilExt <poonam.patil@extetnia.com>
2017-06-22 11:33:20 +0530

    Test data

--- src/Travel.Connectors.Hotel/Controllers/Values.cs
+++ src/Travel.Connectors.Hotel/Controllers/Values.cs
@@ -15,7 +15,7 @@ namespace Travel.Connectors.Hotel.Controllers
         public IEnumerable<string> Get(string test)
         {
             //testData();
-            return new string[] { "value1", "value2 test data"};
+            return new string[] { "value1", "value2 test data 1"};
         }
 
         // GET api/values/5
